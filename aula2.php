f758bbeefd010ca0a5fc1c8bb8bbea7bf6b714c7
Leonardo <leonardo.carvalho.2014@outlook.com>
2021-02-15 20:43:37 -0300

    Tipos de dados

---
+++ aula2.php
@@ -0,0 +1,31 @@
+<?php
+
+$nome = "Leonardo";
+
+echo $nome;
+
+// Tipos de dados 
+/*
+inteiro - Valores absolutos
+Ponto flutuante (double) - Valores fracionais
+string - Texto
+array - ??
+objeto - animais, objetos
+booleanos - true or false
+*/
+
+$idade = 28;
+$pi = 3.14;
+$nome = "Leonardo";
+$acesso = true;
+
+echo $idade."<br>";
+echo $pi;."<br>";
+echo $nome;."<br>";
+eco $acesso;."<br>";
+
+
+
+
+
+?>
