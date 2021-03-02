91ebf10468ba1aa5d765eed06c99145e1a9f3ef1
Leonardo <leonardo.carvalho.2014@outlook.com>
2021-03-01 22:16:16 -0300

    Laços de Repetição

---
+++ while.php
@@ -0,0 +1,20 @@
+<?php
+/*$x = 1;
+$nome   = "Leo";
+
+while ($x <= 500) {
+/*if ($x % 2 == 1) {*/
+
+         /*echo "Nome: $nome e Número: $x <br>";
+
+    $x++;
+
+    }*/
+
+
+    $número = array (1, 2, 3, 4, 5, 6);
+    foreach ($número as $número){
+
+        echo $número."<br>";
+    }
+?>
